fe1a343ecb7a17a6b750489098c0bc86e485774d
EttNollEtt <33880192+EttNollEtt@users.noreply.github.com>
2017-12-05 05:20:02 +0100

    Update projektv2.ino

--- projektv2.ino
+++ projektv2.ino
@@ -1,33 +1,36 @@
-const int trigOne = 7;
-const int echoOne = 8;
-const int trigTwo = 3;
-const int echoTwo = 4;
-const int irOne = 13;
-const int irTwo = 12;
-const int in1 = 6;
-const int in2 = 5;
-const int in3 = 10;
-const int in4 = 11;
+#define trigOne 7
+#define echoOne 8
+#define trigTwo 3
+#define echoTwo 4
+#define irOne 13
+#define irTwo 12
+#define in1 6
+#define in2 5
+#define in3 10
+#define in4 11
 
 // abandon all hope, ye who enter
 
 void systemFail()
+// ifall båda ir-sensorerna upptäcker färg
+// backar den i 1(en) sekund och stannar sedan
+// sedan börjar loopen om
 {
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   digitalWrite(in3, LOW);
   digitalWrite(in4, HIGH);
-  delay(2000);
+  delay(1000);
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   digitalWrite(in3, LOW);
   digitalWrite(in4, LOW);
 }
 
-int distanceRead(int x, int y)
+long distanceRead(int x, int y)
+// tar in en trigpin och en echopin
+// och räknar ut avståndet från sensorn i cm
 {
-  // tar in en trigpin och en echopin
-  // och räknar ut avståndet från sensorn i mm
   int var;
   int distance;
   digitalWrite(x, LOW);
@@ -44,64 +47,60 @@ int distanceRead(int x, int y)
   return distance;
 }
 
-int sensorRead(int x, int y)
+int colorRead(int x)
+// tar in värden från en ir-sensor och
+// jämför det med värdet efter en kort delay
+// den kommer att reagera om skillnaden är för stor 
 {
-  // tar in två IRsensorer och returnerar
-  // värden beroende på vad dem läser av
-  if ((digitalRead(x) == 0) && (digitalRead(y) == 0))
+  int val;
+  int ny;
+  val = analogRead(x);
+  delay(5);
+  ny = analogRead(x);
+  if ((val - ny) > 50) // DEN SJUNDE DAGEN VILADE HAN: DEN SJÄTTE DAGEN SADE HAN; VAR E LJUSET FÖR FAN? OCH LJUSET KOM I SPRUTLUDRET
   {
-    return 0;
+    return true;
   }
-  else if ((digitalRead(x) != 0) && (digitalRead(y) == 0))
+  else
   {
-    return 1;
-  }
-  else if ((digitalRead(x) == 0) && (digitalRead(y) != 0))
-  {
-    return -1;
-  }
-  else if ((digitalRead(x) != 0) && (digitalRead(y) != 0))
-  {
-    systemFail();
+    return false;
   }
 }
 
 void motorControl(int x)
+// slutfunktionen som tar in 1, -1 eller 0
+// som hör ihop med sväng höger, vänster eller rakt fram
+// om irTwo ej upptäcker körs denna om allt annat stämmer
 {
-  // slutfunktionen som tar in 1, -1 eller 0
-  // som hör ihop med sväng höger, vänster eller rakt fram
-  //Serial.print(x);
-  // om irTwo ej upptäcker körs denna om allt annat stämmer
-  //Serial.print(x);
   if (x == 1)
   {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, LOW);
-    digitalWrite(in4, HIGH);
+    analogWrite(in1, 255);
+    analogWrite(in2, 0);
+    analogWrite(in3, 0);
+    analogWrite(in4, 255);
   }
   // om irOne ej upptäcker körs denna om allt annat stämmer
   else if (x == -1)
   {
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
+    analogWrite(in1, 0);
+    analogWrite(in2, 255);
+    analogWrite(in3, 255);
+    analogWrite(in4, 0);
   }
   // om båda IR upptäcker mark körs denna om allt annat stämmer
   else if (x == 0)
   {
-    digitalWrite(in1, HIGH);
-    digitalWrite(in2, LOW);
-    digitalWrite(in3, HIGH);
-    digitalWrite(in4, LOW);
+    analogWrite(in1, 255);
+    analogWrite(in2, 0);
+    analogWrite(in3, 255);
+    analogWrite(in4, 0);
   }
 }
 
 int deltaDistance(int x, int y)
+// kollar om avståndet till den ena sensorn är större än det andra avståndet
+// isåfall och skillnaden är större än 15cm så svänger den
 {
-  // kollar om avståndet till den ena sensorn är större än det andra avståndet
-  // isåfall och skillnaden är större än 15cm så svänger den
   Serial.print(x);
   if (((x > y)) && ((x - y) > 5))
   {
@@ -117,7 +116,8 @@ int deltaDistance(int x, int y)
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -131,25 +131,30 @@ void setup() {
   pinMode(irTwo, INPUT);
 }
 
-void loop() {
+void loop()
+// kollar med ir-sensorerna först av allt
+// beroende på vad de känner av så kommer
+// en viss funktion med motorerna utföras
+{
   delay(10);
   int distanceOne;
   int distanceTwo;
-  int sensor;
-  sensor = sensorRead(irOne, irTwo);
-  if (sensor == 0)
+  if (colorRead(irOne) == true && colorRead(irTwo) == true)
+  {
+    systemFail();
+  }
+  else if ((colorRead(irOne) &&! colorRead(irTwo) == true))
+  {
+    motorControl(1);
+  }
+  else if ((colorRead(irTwo) &&! colorRead(irOne) == true))
+  {
+    motorControl(-1);
+  }
+  else
   {
     distanceOne = distanceRead(trigOne, echoOne);
     distanceTwo = distanceRead(trigTwo, echoTwo);
     deltaDistance(distanceOne, distanceTwo);
   }
-  else if (sensor == 1)
-  {
-    motorControl(1);
-  }
-  else
-  {
-    motorControl(-1);
-  }
 }
-
